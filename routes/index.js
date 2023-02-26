e6b25ce0906cc3de66648882295303ac814d24bd
entrydenied <benjamintak17@gmail.com>
2023-02-26 13:04:35 -0800

    Simple message array

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Bob",
+    add: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Chuck",
+    add: new Date()
+  }
+];
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
