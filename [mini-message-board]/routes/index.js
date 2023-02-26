ea961fc509b7cc9ebbabdb2164f1f04980c99f19
entrydenied <benjamintak17@gmail.com>
2023-02-26 14:41:15 -0800

    Add sample users in const messages

--- [mini-message-board]/routes/index.js
+++ [mini-message-board]/routes/index.js
@@ -24,7 +24,18 @@
 var express = require('express');
 var router = express.Router();
 
-var messages = [];
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
