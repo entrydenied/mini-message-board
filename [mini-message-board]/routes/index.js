30c4ad585de83eca88101d798f68f4bc313b99d7
entrydenied <benjamintak17@gmail.com>
2023-02-26 13:23:34 -0800

    Properly generated pug files

--- [mini-message-board]/routes/index.js
+++ [mini-message-board]/routes/index.js
@@ -1,19 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-const messages = [
-  {
-    text: "Hi there!",
-    user: "Bob",
-    add: new Date()
-  },
-  {
-    text: "Hello World!",
-    user: "Chuck",
-    add: new Date()
-  }
-];
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
