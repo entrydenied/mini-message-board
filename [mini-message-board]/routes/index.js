deecff494855e8068b016bc7dcaefe6596028158
entrydenied <benjamintak17@gmail.com>
2023-02-26 13:45:35 -0800

    Add message loop

--- [mini-message-board]/routes/index.js
+++ [mini-message-board]/routes/index.js
@@ -6,4 +6,15 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// STEP 5 LOOP 
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Mini Messageboard', messages: messages });
+});
+
+// STEP 6
+router.get('/new', function(req, res, next) {
+  res.render('form', { title: 'New Message' });
+});
+
+
 module.exports = router;
