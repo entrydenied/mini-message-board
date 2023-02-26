18a2b2bb8b2ddaa14b70e707170f646efba21a88
entrydenied <benjamintak17@gmail.com>
2023-02-26 15:24:36 -0800

    Remove old commented code

--- [mini-message-board]/routes/index.js
+++ [mini-message-board]/routes/index.js
@@ -1,26 +1,3 @@
-// var express = require('express');
-// var router = express.Router();
-
-// var messages = [];
-
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// // // STEP 5 LOOP 
-// // router.get('/', function(req, res, next) {
-// //   res.render('index', { title: 'Mini Messageboard', messages: messages });
-// // });
-
-// // STEP 6
-// router.get('/new', function(req, res, next) {
-//   res.render('form', { title: 'New Message' });
-// });
-
-
-// module.exports = router;
-
 var express = require('express');
 var router = express.Router();
 
