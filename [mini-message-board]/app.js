b32936423f3460b4c7bbe9930f3a0cb0917c9261
entrydenied <benjamintak17@gmail.com>
2023-02-26 13:56:06 -0800

    Add app.set('view engine', 'pug');

--- [mini-message-board]/app.js
+++ [mini-message-board]/app.js
@@ -8,6 +8,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+app.set('view engine', 'pug');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
